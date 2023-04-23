de6a44c2a4e7bff91794bc0d14c59ef58ad1c0df
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2023-04-23 22:21:12 +0900

    Feat: 카카오 로그인 버튼 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,32 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
+import Button from '@/components/Common/Button';
+
 function Login() {
-  return <div> LOGIN </div>;
+  const router = useRouter();
+
+  const handleKaKaoLogin = () => {
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&response_type=code`;
+
+    router.push(KAKAO_AUTH_URL);
+  };
+
+  if (typeof window !== 'object') return <></>;
+
+  return (
+    <section>
+      <div>
+        <Button
+          title="카카오 로그인"
+          imgUrl="Kakao.png"
+          bgColor="#fae100"
+          onClick={handleKaKaoLogin}
+        />
+      </div>
+    </section>
+  );
 }
 
 export default Login;
