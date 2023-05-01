e3f6169cceefdbc03ca3fa2b670ce3e1c910aa55
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2023-05-01 11:49:23 +0900

    fix: https에서 http 요청 문제 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
       <body>
         <Header />
         <main className="max-w-7xl m-auto relative max-h-screen flex flex-col items-center h-full">
