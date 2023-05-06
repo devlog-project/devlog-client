de12b74a88df02db6085b474e5764999796a88a5
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2023-05-07 01:40:58 +0900

    feat: 회원가입 페이지 템플릿 추가

---
+++ src/components/Signup/templates/SignupTemplates.tsx
@@ -0,0 +1,9 @@
+function SignupTemplates() {
+  return (
+    <section className="container h-full mt-40 relative flex flex-col items-center justify-center h-full gap-5">
+      회원가입 페이지!
+    </section>
+  );
+}
+
+export default SignupTemplates;
